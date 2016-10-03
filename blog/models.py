b52b903bd92ff8b941463d5e6dd047dbf9de81b1
admin <johnycaprice@protonmail.com>
2016-10-03 18:34:41 -0400

    Changed the HTML for the site

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils import timezone 
+from django.utils import timezone
 
 class Post(models.Model):
 	author = models.ForeignKey('auth.User')
@@ -9,15 +9,20 @@ class Post(models.Model):
 			default=timezone.now)
 	published_date = models.DateTimeField(
 			blank=True, null=True)
-	
-	
+
+
 	def publish(self):
 		self.published_date = timezone.now()
 		self.save()
 
 	def __str__(self):
-		return self.title
-	
+		return self.titlefdfdadfadd
+
+
+
+
+
+
 
 
 
